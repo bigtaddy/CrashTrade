cc510e7028bf3ea15d152af00fb15a89f09a01f5
Dmitry <petka777794@gmail.com>
2015-12-17 22:40:57 +0300

    delete tabs.js

--- ResourceMetadata.Web/Scripts/tabs.js
+++
@@ -1,9 +0,0 @@
-
-    $('.nav-tabs li a').on('click', function () {
-        $(this).parent().find('.active').removeClass('active');
-        $(this).addClass('active');
-    });
-
-$('.text-new-line').trunk8({
-    lines: 6
-});
